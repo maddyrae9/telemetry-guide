212d32e3e94dcb9d99f7b3e11d224a3facac4b61
Madison Mulligan <madisonmulligan97@gmail.com>
2023-09-09 21:59:02 -0400

    Added components, assets, and utils folder

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Telemetry Guide
         </a>
       </header>
     </div>
